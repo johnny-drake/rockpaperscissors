d2aec6967f1805b22b53ce00ed1321c895cba35c
johnny-drake <drakejohnny8@gmail.com>
2022-01-26 21:22:04 +0000

    moo

--- script.js
+++ script.js
@@ -1,114 +1,95 @@
+document.getElementById("rock").addEventListener("click", playRock);
+document.getElementById("paper").addEventListener("click", playPaper);
+document.getElementById("scissors").addEventListener("click", playScissors);
 
+computerScore=0;
+playerScore=0;
 
+function playRock() {
 
-
-
-function test() {
-    
-    let Result = ""
-
-    let playerChoices = prompt("Please enter Rock, Paper or Scissors")
-    
     let computerChoices = Math.random();
     if (computerChoices<0.3333333333) {
-    computerChoices="Rock";
+        document.getElementById("outcome").innerHTML="Computer chooses rock! Its a tie!";
     } else if (computerChoices>0.67) {
-    computerChoices="Paper";
+        document.getElementById("outcome").innerHTML="Computer chooses paper! Computer wins!"
+        computerScore=computerScore+1;
     } else { 
-    computerChoices="Scissors"
+        document.getElementById("outcome").innerHTML="Computer chooses scissors! Player wins!"
+        playerScore=playerScore+1;
     }
 
+    document.getElementById("computerScore").innerHTML = "Computer: " + computerScore;
+    document.getElementById("playerScore").innerHTML = "Player: " + playerScore;
 
-
-    if (playerChoices=="Rock" && computerChoices=="Rock") {
-        Result = ("Computer chooses Rock! It's a tie!")
-        } else if (playerChoices=="Rock" && computerChoices=="Paper") {
-        Result = ("Computer chooses Paper! Paper beats Rock! Computer Wins!")
-        } else if (playerChoices=="Rock" && computerChoices=="Scissors") {
-        Result = ("Computer chooses Scissors! Rock beats Scissors! Player Wins!")
-        } else if (playerChoices=="Paper" && computerChoices=="Rock") {
-        Result = ("Computer chooses Rock! Paper beats Rock! Player Wins!")
-        } else if (playerChoices=="Paper" && computerChoices=="Scissors") {
-        Result = ("Computer chooses Scissors! Scissors beats Paper! Computer Wins!")
-        } else if (playerChoices=="Paper" && computerChoices=="Paper") { 
-        Result = ("Computer chooses Paper! It's a tie!")
-        } else if (playerChoices=="Scissors" && computerChoices=="Rock") {
-        Result = ("Computer chooses Rock! Rock beats Scissors! Computer Wins!")
-        } else if (playerChoices=="Scissors" && computerChoices=="Paper") {
-        Result = ("Computer chooses Paper! Scissors beats Paper! Player Wins!")
-        } else if (playerChoices=="Scissors" && computerChoices=="Scissors") {
-        Result = ("Computer chooses Scissors! It's a tie!");
-        } else {
-        }
-        
-
-        if (playerChoices=="Rock" && computerChoices=="Rock") {
-            
-            } else if (playerChoices=="Rock" && computerChoices=="Paper") {
-            computerScore=computerScore+1
-            } else if (playerChoices=="Rock" && computerChoices=="Scissors") {
-                playerScore=playerScore+1
-            } else if (playerChoices=="Paper" && computerChoices=="Rock") {
-                playerScore=playerScore+1
-            } else if (playerChoices=="Paper" && computerChoices=="Scissors") {
-                computerScore=computerScore+1
-            } else if (playerChoices=="Paper" && computerChoices=="Paper") { 
-                
-            } else if (playerChoices=="Scissors" && computerChoices=="Rock") {
-                computerScore=computerScore+1
-            } else if (playerChoices=="Scissors" && computerChoices=="Paper") {
-                playerScore=playerScore+1
-            } else if (playerChoices=="Scissors" && computerChoices=="Scissors") {
-                
-            } else {
-            }
-            return Result;
-
+    if (computerScore === 5) {
+        computerScore = computerScore*0
+        playerScore = playerScore*0
+        document.getElementById("outcome").innerHTML="Computer is first to 5 points! Computer is the winner!"
+    } else if (playerScore === 5) {
+        computerScore = computerScore*0
+        playerScore = playerScore*0
+        document.getElementById("outcome").innerHTML="Player is first to 5 points! Player is the winner!"
     }
 
+}
 
+function playPaper() {
 
+    let computerChoices = Math.random();
+    if (computerChoices<0.3333333333) {
+        document.getElementById("outcome").innerHTML="Computer chooses paper! Its a draw!";
+    } else if (computerChoices>0.67) {
+        document.getElementById("outcome").innerHTML="Computer chooses scissors! Computer wins!";
+        computerScore=computerScore+1;
+    } else { 
+        document.getElementById("outcome").innerHTML="Computer chooses rock! Player wins!"
+        playerScore=playerScore+1;
+    }
 
-    
-function game() {
-    computerScore=0
-    playerScore=0
+    document.getElementById("computerScore").innerHTML = "Computer: " + computerScore;
+    document.getElementById("playerScore").innerHTML = "Player: " + playerScore;
 
-    console.log(test())
-    console.log(test())
-    console.log(test())
-    console.log(test())
-    console.log(test())
+    if (computerScore === 5) {
+        computerScore = computerScore*0
+        playerScore = playerScore*0
+        document.getElementById("outcome").innerHTML="Computer is first to 5 points! Computer is the winner!"
+    } else if (playerScore === 5) {
+        computerScore = computerScore*0
+        playerScore = playerScore*0
+        document.getElementById("outcome").innerHTML="Player is first to 5 points! Player is the winner!"
+    }
 
+}
 
+function playScissors() {
 
-let message = ""
+    let computerChoices = Math.random();
+    if (computerChoices<0.3333333333) {
+     document.getElementById("outcome").innerHTML="Computer chooses scissors! Its a draw!";
+    } else if (computerChoices>0.67) {
+        document.getElementById("outcome").innerHTML="Computer chooses rock! Computer wins!";
+        computerScore=computerScore+1;
+    } else { 
+        document.getElementById("outcome").innerHTML="Computer chooses paper! Player wins!"
+        playerScore=playerScore+1;
+    } 
 
-if (computerScore>playerScore){
-    message="Computer is the winner!"
-} else if (computerScore<playerScore) {
-    message="Player is the winner!"
-} else 
-    message="It's a tie!"
+    document.getElementById("computerScore").innerHTML = "Computer: " + computerScore;
+    document.getElementById("playerScore").innerHTML = "Player: " + playerScore;
 
-
-
-    return [message, computerScore, playerScore];
+    if (computerScore === 5) {
+        computerScore = computerScore*0
+        playerScore = playerScore*0
+        document.getElementById("outcome").innerHTML="Computer is first to 5 points! Computer is the winner!"
+    } else if (playerScore === 5) {
+        computerScore = computerScore*0
+        playerScore = playerScore*0
+        document.getElementById("outcome").innerHTML="Player is first to 5 points! Player is the winner!"
+    }
 
 }
 
 
-
-
-
-
-
-
-
-
-
-
-  
 
 
 
